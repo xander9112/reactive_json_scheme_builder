3da24fab977a285e767aa31025f141e7c97b267e
alllll <alllll.perm@gmail.com>
2025-08-22 11:21:51 +0500

    Update number_control.dart

--- lib/src/presentation/reactive/controls/number_control.dart
+++ lib/src/presentation/reactive/controls/number_control.dart
@@ -26,36 +26,30 @@ class ReactiveNumberControl extends StatefulWidget {
 class _ReactiveNumberControlState extends State<ReactiveNumberControl> {
   @override
   Widget build(BuildContext context) {
-    return ReactiveFormField(
-      formControlName: widget.formControlName,
-      builder: (field) {
-        final control = JsonSchemeFormControl.fromFormControl(field.control);
-        if (!control.visible) {
-          return const SizedBox();
-        }
-
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 16.0),
-          child: ReactiveTextField<num>(
-            formControlName: widget.formControlName,
-            inputFormatters: [
-              // DecimalThousandsFormatter(locale: 'ru_RU'),
-              // FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
-              FilteringTextInputFormatter.allow(
-                RegExp(
-                  r'^\d*[.,]?\d*$',
-                ),
-              ), // Чтобы можно было вводить и 12.34 и 12,34
-            ],
-            keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            valueAccessor: NumValueAccessor(
-                 // locale: 'ru_RU', // или другой локаль
-                 ),
-            decoration: InputDecoration(
-              labelText: widget.label,
-              helperText: widget.description,
-              border: const OutlineInputBorder(),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 16.0),
+      child: ReactiveTextField<num>(
+        formControlName: widget.formControlName,
+        inputFormatters: [
+          // DecimalThousandsFormatter(locale: 'ru_RU'),
+          // FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+          FilteringTextInputFormatter.allow(
+            RegExp(
+              r'^\d*[.,]?\d*$',
             ),
+          ), // Чтобы можно было вводить и 12.34 и 12,34
+        ],
+        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+        valueAccessor: NumValueAccessor(
+            //     // locale: 'ru_RU', // или другой локаль
+
+            ),
+        decoration: InputDecoration(
+          labelText: widget.label,
+          helperText: widget.description,
+          border: const OutlineInputBorder(),
+        ),
+      ),
     );
   }
 }
